d827a88dbaf287eee78e6e5cc3c467d313b00652
vertilang <3227186874@qq.com>
2024-01-11 12:57:23 +0800

    1

--- include/predict.h
+++ include/predict.h
@@ -8,6 +8,8 @@
 #include <sys/time.h>
 #include <thread>
 #include <termio.h>
+#include <ceres/jet.h>
+#include <ceres/ceres.h>
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Eigen>
@@ -173,6 +175,103 @@ enum class PREDICTORMODE{
         bool is_large_;
 
     };
+    /**
+    * @brief  自适应扩展卡尔曼滤波, 花山甲老师写的自适应扩展卡尔曼滤波实在优雅
+    * @author 上交:唐欣阳
+    */
+    template <int N_X, int N_Y>
+    class AdaptiveEKF
+    {   
+        using MatrixXX = Eigen::Matrix<double, N_X, N_X>;
+        using MatrixYX = Eigen::Matrix<double, N_Y, N_X>;
+        using MatrixXY = Eigen::Matrix<double, N_X, N_Y>;
+        using MatrixYY = Eigen::Matrix<double, N_Y, N_Y>;
+        using VectorX = Eigen::Matrix<double, N_X, 1>;
+        using VectorY = Eigen::Matrix<double, N_Y, 1>;
+
+    public:
+        explicit AdaptiveEKF(const VectorX &X0 = VectorX::Zero())
+            : Xe(X0), P(MatrixXX::Identity())
+        {
+            std::cout << P << std::endl;
+            Q << 0.15 ,0,    0,     0,  0,  0,
+                0,    1,  0,     0,  0,  0,
+                0,    0,    0.1,   0,  0,  0,
+                0,    0,    0,    0.3, 0,  0,
+                0,    0,    0,     0,  0.5,0,
+                0,    0,    0,     0,  0,  1;
+
+            R << 4,0,0,
+                0,1,0,
+                0,0,0.5;
+        }
+    
+        void init(const VectorX &X0 = VectorX::Zero()) 
+        {
+            Xe = X0;
+        }
+
+        template <class Func>
+        VectorX predict(Func &&func)
+        {
+            ceres::Jet<double, N_X> Xe_auto_jet[N_X];
+
+            for (int i = 0; i < N_X; i++)
+            {
+                Xe_auto_jet[i].a = Xe[i];
+                Xe_auto_jet[i].v[i] = 1;
+            }
+
+            ceres::Jet<double, N_X> Xp_auto_jet[N_X];
+            func(Xe_auto_jet, Xp_auto_jet);
+            for (int i = 0; i < N_X; i++)
+            {
+                Xp[i] = Xp_auto_jet[i].a;
+                F.block(i, 0, 1, N_X) = Xp_auto_jet[i].v.transpose();
+            }
+            std::cout << F * P * F.transpose() << std::endl;
+            P = F * P * F.transpose() + Q;
+            std::cout << "predict variables is x: " << Xp[0] << " y: " << Xp[2] << " z: " << Xp[4] << std::endl; 
+
+            return Xp;
+        }
+
+        template <class Func>
+        VectorX update(Func &&func, const VectorY &Y)
+        {
+            ceres::Jet<double, N_X> Xp_auto_jet[N_X];
+            for (int i = 0; i < N_X; i++)
+            {
+                Xp_auto_jet[i].a = Xp[i];
+                Xp_auto_jet[i].v[i] = 1;
+            }
+            ceres::Jet<double, N_X> Yp_auto_jet[N_Y];
+            func(Xp_auto_jet, Yp_auto_jet);
+            for (int i = 0; i < N_Y; i++)
+            {
+                Yp[i] = Yp_auto_jet[i].a;
+                H.block(i, 0, 1, N_X) = Yp_auto_jet[i].v.transpose();
+            }
+            K = P * H.transpose() * (H * P * H.transpose() + R).inverse();
+
+            Xe = Xp + K * (Y - Yp);
+            P = (MatrixXX::Identity() - K * H) * P;
+
+            std::cout << "update variables is x: " << Xe[0] << " y: " << Xe[2] << " z: " << Xe[4] << std::endl; 
+            return Xe;
+        }
+
+        VectorX Xe; // 估计状态变量
+        VectorX Xp; // 预测状态变量
+        MatrixXX F; // 预测雅克比
+        MatrixYX H; // 观测雅克比
+        MatrixXX P; // 状态协方差
+        MatrixXX Q; // 预测过程协方差
+        MatrixYY R; // 观测过程协方差
+        MatrixXY K; // 卡尔曼增益
+        VectorY Yp; // 预测观测量
+    };
+
     class predictor
     {
     
